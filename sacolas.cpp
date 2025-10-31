97c1ec56c4ad64615f748f9dd4cb88cb23b89eda
Otávio Pinheiro <otaviorp2014@gmail.com>
2025-10-31 12:07:19 -0300

    Create sacolas.cpp

---
+++ sacolas.cpp
@@ -0,0 +1,21 @@
+// NOIC - Tutorial
+// Otávio Pinheiro
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, S; cin >> n >> S;
+
+    int cnt = 1, peso = 0;
+    for (int i = 0; i < n; i++) {
+        int atual; cin >> atual;
+      //se o peso do produto atual não pode na sacola, pegamos outra e colocamos esse produto
+        if(peso + atual > S) {
+            cnt++;
+            curSum = atual;
+        } else curSum += atual;
+    }   
+
+    cout << cnt << '\n';
+    return 0;
+}
